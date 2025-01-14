671e46ae3a3c187b7ac2d5427da49dd1c907fff0
SHIN BEOM SU <qjatn147852@naver.com>
2025-01-14 13:18:14 +0900

    250114

---
+++ Assets/Scripts/24.07new/Controller/SystemController/Dungeon/WestField/WestFieldTimelineFinished.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class WestFieldTimelineFinished : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector pd;
+    [SerializeField] private GameObject timelineSettings;
+    
+    void OnEnable() 
+    {
+        pd.stopped+=OnTimelineFinished;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    void OnTimelineFinished(PlayableDirector pd)
+    {
+        pd.gameObject.SetActive(false);
+        timelineSettings.SetActive(false);
+    }
+}
