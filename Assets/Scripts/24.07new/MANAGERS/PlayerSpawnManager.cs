b190990f285b061330febf814c6d1bab07ffef1e
SHIN BEOM SU <qjatn147852@naver.com>
2025-01-15 15:07:18 +0900

    250115_씬 로드 로직 대폭 수정
    1. 비동기로딩씬 일단 제거
    ->스폰포인트 제대로 잡고 이후에 다시 추가하던지 함
    2. PlayerSpawnPoint 수정 : 하드코딩도 안됨. 도대체 왜 스폰 포인트가 안잡아지는지 도무지 모르겠음

--- Assets/Scripts/24.07new/MANAGERS/PlayerSpawnManager.cs
+++ Assets/Scripts/24.07new/MANAGERS/PlayerSpawnManager.cs
@@ -8,55 +8,44 @@ public class PlayerSpawnManager : MonoBehaviour
     //씬마다 스폰되는 플레이어 위치를 제어하기 위한 스크립트.
 
     [SerializeField] private GameObject Player;
-    private GameObject SpawnPoint;
-
-    private void Awake() 
-    {
-        // 씬 전환 시 PlayerSpawnManager가 다른 씬으로 유지되도록 설정
-        DontDestroyOnLoad(gameObject);
-
-        // 씬 로드 시마다 스폰 포인트를 설정하도록 이벤트 구독
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
     
+    private void Awake()
+    {
+        if(Player==null)
+        {
+            Player = GameObject.FindWithTag("Player");
+        }
+        SceneManager.sceneLoaded+=OnSceneLoaded;   
+    }
+
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        // 새로운 씬이 로드될 때마다 스폰 포인트를 설정합니다.
-        SetSpawnPoint(scene.name);
-    }
-
-    private void SetSpawnPoint(string sceneName)
-    {
-        // 각 씬마다 스폰되어야 할 위치 오브젝트를 찾는다.(Empty Object)
-         switch (sceneName)
+        GameObject spawnPoint = null;
+        if(scene.name == "Olenamento")
         {
-            case "Olenamento_WestField":
-                SpawnPoint = GameObject.Find("WestFieldSpawnPoint");
-                break;
-            case "Olenamento":
-                SpawnPoint = GameObject.Find("OlenamentoSpawnPoint");
-                break;
-            case "Olenamento_WestField_BossStage":
-                SpawnPoint = GameObject.Find("WestBossStageSpawnPoint");
-                break;
-            default:
-                Debug.LogWarning($"[{sceneName}]에 대한 스폰 포인트가 정의되지 않았습니다.");
-                return;
+            spawnPoint = GameObject.Find("OlenamentoSpawnPoint");  
+        }
+        else if(scene.name == "Olenamento_WestField")
+        {
+            spawnPoint = GameObject.Find("WestFieldSpawnPoint");
+        }
+        else if(scene.name == "Olenamento_WestField_BossStage")
+        {
+            spawnPoint = GameObject.Find("WestBossStageSpawnPoint");
         }
 
-        if(SpawnPoint!=null)
-        {
-            Player.transform.position = SpawnPoint.transform.position;//스폰포인트가 존재하면 그 위치로 스폰시킨다.
-            Debug.Log($"Spawn Point : {SpawnPoint.name}");
-        }
-        else
-        {
-            Debug.LogWarning($"{sceneName}에서 스폰 포인트를 찾지 못함.");
-        }
+        SetSpawnPosition(spawnPoint);
     }
-    private void OnDestroy()
+
+    private void SetSpawnPosition(GameObject sp)
     {
-        // 씬이 변경될 때 이벤트 구독을 해제
-        SceneManager.sceneLoaded -= OnSceneLoaded;
+        if(sp!=null)
+        {
+            Player.transform.position = sp.transform.position;
+            Player.transform.rotation = sp.transform.rotation;
+            Debug.Log($"player.transform.position : {Player.transform.position}, sp position : {sp.transform.position}");
+        }
     }
+
+
 }
